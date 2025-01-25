2ffe905e5119290008b5b919bd954491c53edec7
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2025-01-24 20:24:28 -0800

    knocking off for tonight

--- ablate.py
+++ ablate.py
@@ -3,10 +3,15 @@
 # https://zheng-dai.github.io/AblationBasedCounterfactuals/
 # https://github.com/zheng-dai/GenEns
 
+# Check out that diff: tensor(6376.6074, device='mps:0', grad_fn=<AddBackward0>) tensor(34779.3555, device='mps:0', grad_fn=<AddBackward0>)
+
 import torch
 import torch.nn as nn
+import torchvision
 from architecture import Simple_VAE
 from dataset import get_train_dataset, get_dataloader
+import numpy as np
+import matplotlib.pyplot as plt
 device = "mps" if torch.backends.mps.is_available() else "cpu"
 
 run = "runs/vae_test1"
@@ -26,7 +31,16 @@ def get_loss(image, net):
 
     return loss
 
-def ablate(image, net,thresh=0.1):
+
+@torch.no_grad()
+def loss_recon_package(image, net):
+    reconstructed, mean, logvar = net(image)
+
+    loss = net.loss_function(reconstructed, image, mean, logvar)
+
+    return loss.item(), reconstructed
+
+def ablate(image, net,thresh=0.6):
     
     net.eval()
         
@@ -39,22 +53,44 @@ def ablate(image, net,thresh=0.1):
     with torch.no_grad():
         for param in net.parameters():
             if param.grad is not None:
-                param.grad[torch.abs(param.grad) > thresh] = 0
+                param[torch.abs(param.grad) < thresh] = 0
+
+def before_after(item, net):
+    before_loss, before_recon = loss_recon_package(item, net)
+
+    ablate(item, net)
+
+    after_loss, after_recon = loss_recon_package(item, net)
+
+    return before_loss, before_recon, after_loss, after_recon
 
 
 trainset = get_train_dataset()
 
 dataloader = get_dataloader(trainset)
 
-for item, _ in dataloader:
+for dummy_item, _ in dataloader:
+    dummy = dummy_item.to(device)
+
+    before_dummy_loss, before_dummy_recon = loss_recon_package(dummy, net)
     
-    before = get_loss(item.to(device), net)
+    break
 
-    ablate(item.to(device), net)
+for item, _ in dataloader:
 
-    after = get_loss(item.to(device), net)
+    before_loss, before_recon, after_loss, after_recon = before_after(item.to(device), net)
 
-    print(before, after)
+    item = item.to(device)
+
+    dummy_loss, dummy_recon = loss_recon_package(dummy, net)
+
+
+    print(before_loss, after_loss)
+    print(before_dummy_loss, dummy_loss)
+    grid = torchvision.utils.make_grid([item[0], before_recon[0], after_recon[0], dummy[0], before_dummy_recon[0], dummy_recon[0]], nrow=3).cpu().numpy()
+    plt.imshow(np.transpose(grid, (1, 2, 0)))
+    plt.show()
+
 
     break
     
